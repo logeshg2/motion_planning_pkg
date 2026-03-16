822c3e36848feb5c95106b3752883550eda1958d
Logesh <logeshg248@gmail.com>
2026-03-16 22:36:42 +0530

    mujoco ur - path planner simle eg

---
+++ examples/mujoco_eg.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+"""Example script - motion plan robotic arm in a Mujoco Env"""
+
+
+import time
+import mujoco
+import numpy as np
+from mujoco.viewer import launch_passive
+from scipy.spatial.transform import Rotation
+
+from mpkg.diff_ik import IK_Solver
+from mpkg.rrt.rrt_star import RRTStarPlanner
+from mpkg.utils.pin_utils import loadModelFromMJCF
+from mpkg.utils.base_utils import createPoseTransform
+
+
+UR5_XML_PATH = "/home/logesh/mujoco_ws/mujoco_menagerie/universal_robots_ur5e/ur5e.xml"
+SCENE_PATH = "/home/logesh/mujoco_ws/mujoco_menagerie/universal_robots_ur5e/scene.xml"
+
+
+### mujoco setup
+
+mjModel = mujoco.MjModel.from_xml_path(SCENE_PATH)
+mjData = mujoco.MjData(mjModel)
+
+dt = 0.02
+mjModel.opt.timestep = dt
+
+# get all actuator name (no gripper the choosen ur5 robotic arm)
+act_names = []
+for id in range(mjModel.nu):
+    act_names.append(str(mjModel.actuator(id).name))
+home_qpos = mjModel.key("home").qpos
+home_key_id = mjModel.key("home").id
+
+###
+
+### Pinocchio model
+
+model, collision_model, visual_model = loadModelFromMJCF(UR5_XML_PATH)
+# TODO: need to add self collision
+
+###
+
+
+### IK Solver
+
+ik_solver = IK_Solver(model, collision_model, visual_model, "attachment_site", visualize_final_config=False)
+targetPose = createPoseTransform([0.3, 0.4, 0.3], [-180, 0, 0])
+des_q = ik_solver.solve_ik(np.zeros((6,)), targetPose)
+
+if (des_q is None):
+    print("Cannot find solution!")
+    exit(0)
+
+###
+
+
+###
+
+# rrt star planner
+planner = RRTStarPlanner(
+    model, 
+    collision_model, 
+    visual_model,
+    "attachment_site",
+    rng_seed=42,
+    visualize=False,
+    verbose=False
+)
+start_q = home_qpos
+goal_q = des_q
+
+# compute path
+config_path = planner.plan(start_config = start_q, goal_config = goal_q)
+
+if (config_path is None):
+    print("Cannot find path!")
+    exit(0)
+
+cur_idx = 0
+
+###
+
+
+with launch_passive(mjModel, mjData, show_left_ui=False, show_right_ui=False) as viewer:
+
+    mujoco.mj_resetDataKeyframe(mjModel, mjData, home_key_id)
+    mujoco.mjv_defaultFreeCamera(mjModel, viewer.cam)
+
+    # this will show up frames
+    # viewer.opt.frame = 1
+    # viewer.opt.sitegroup[:] = 1
+
+    while viewer.is_running():
+        start_time = time.time()
+
+        if (cur_idx >= len(config_path)):
+            print("Goal reached!")
+            time.sleep(3)
+            viewer.close()
+        else:
+            mjData.ctrl[:] = config_path[cur_idx]
+
+            if (np.linalg.norm(mjData.qpos - config_path[cur_idx], ord=np.inf) < 0.01):
+                print(f"position-{cur_idx} completed")
+                cur_idx += 1
+
+        mujoco.mj_step(mjModel, mjData)
+        viewer.sync()
+
+        remaining_time = time.time() - start_time
+        if (remaining_time > 0):
+            time.sleep(remaining_time)
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
